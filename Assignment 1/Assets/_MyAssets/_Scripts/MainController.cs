75c125f6ec8f5a6e4324d2b5dec5b57d4333403c
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-02-26 13:48:34 -0500

    UI sprites added, UI in progress

--- Assignment 1/Assets/_MyAssets/_Scripts/MainController.cs
+++ Assignment 1/Assets/_MyAssets/_Scripts/MainController.cs
@@ -9,30 +9,26 @@ public class MainController : MonoBehaviour
 
     private void Awake()
     {
-        // Instanace creation and enforcement of only one objects.
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-            Initialize();
+        //// Instanace creation and enforcement of only one objects.
+        //if (MainController.Instance == null)
+        //{
+        //    SoundManager.Instance = this;
+        //    DontDestroyOnLoad(this);
+        //    Instantiate(MainController.instance);
 
-        }
-        else // If instance already exists and points to an instance of MainController.
-        {
-            Debug.Log("Goodbye cruel world!");
-            Destroy(gameObject); // Destroy the new instance, so only the original remains.
-        }
+        //}
+        //else // If instance already exists and points to an instance of MainController.
+        //{
+        //    Debug.Log("Goodbye cruel world!");
+        //    Destroy(gameObject); // Destroy the new instance, so only the original remains.
+        //}
     }
 
     void Start()
     {  
-        //SoundManager.Instance.AddSound("Boom", Resources.Load<AudioClip>("boom"), SoundManager.SoundType.SOUND_SFX);
-        //SoundManager.Instance.AddSound("Death", Resources.Load<AudioClip>("death"), SoundManager.SoundType.SOUND_SFX);
-        //SoundManager.Instance.AddSound("Jump", Resources.Load<AudioClip>("jump"), SoundManager.SoundType.SOUND_SFX);
-        //SoundManager.Instance.AddSound("Laser", Resources.Load<AudioClip>("laser"), SoundManager.SoundType.SOUND_SFX);
-        //SoundManager.Instance.AddSound("MASK", Resources.Load<AudioClip>("MASK"), SoundManager.SoundType.SOUND_MUSIC);
-        //SoundManager.Instance.AddSound("TCats", Resources.Load<AudioClip>("Thundercats"), SoundManager.SoundType.SOUND_MUSIC);
-        //SoundManager.Instance.AddSound("Turtles", Resources.Load<AudioClip>("Turtles"), SoundManager.SoundType.SOUND_MUSIC);
+        //SoundManager.Instance.AddSound("", Resources.Load<AudioClip>(""), SoundManager.SoundType.SOUND_SFX);
+
+        //SoundManager.Instance.AddSound("", Resources.Load<AudioClip>(""), SoundManager.SoundType.SOUND_MUSIC);
 
         // ADD DONT DESTROY ON LOAD!!!
     }
