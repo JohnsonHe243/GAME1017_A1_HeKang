67f8ce18d33777f9932e3a042ca46c6524b68380
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-03-01 23:28:05 -0500

    commented out UI
    compiler error

--- Assignment 1/Assets/_MyAssets/_Scripts/UIManager.cs
+++ Assignment 1/Assets/_MyAssets/_Scripts/UIManager.cs
@@ -24,7 +24,6 @@ public class ScoreEntry
     public string Date;
     [XmlAttribute("Value")]
     public int Value;
-    public ScoreEntry() { }
     public ScoreEntry(string date, int value)
     {
         Date = date;
@@ -35,80 +34,72 @@ public class ScoreEntry
 public class UIManager : MonoBehaviour
 {
     [SerializeField] public SettingsPopup settingsPopup;
-    [SerializeField] public TMP_Text score;
-    [SerializeField] public TMP_Text date;
+    [SerializeField] public TMP_Text scoreT;
+    [SerializeField] public TMP_Text dateT;
+    private HighScores highScores;
+    public string date;
+    public int score;
     int sceneIndex;
 
-    public static UIManager Instance { get; private set; }
+    //public static UIManager Instance { get; private set; }
 
-    private void Start()
-    {
-        sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (SceneManager.GetActiveScene().buildIndex == 0 || SceneManager.GetActiveScene().buildIndex == 1)
-        {
-            settingsPopup.Close();
-        }
-        if (SceneManager.GetActiveScene().buildIndex == 2)
-        {
-            DisplayScores();
-        }
+//    private void Start()
+//    {
+//        highScores = new HighScores();
+//        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+//        if (SceneManager.GetActiveScene().buildIndex == 0 || SceneManager.GetActiveScene().buildIndex == 1)
+//        {
+//            settingsPopup.Close();
+//        }
+//        if (SceneManager.GetActiveScene().buildIndex == 2)
+//        {
+//            DisplayScores();
+
+//        }
+//        if (sceneIndex == 1 && PlayerMovement.Instance.isAlive == false)
+//        {
+//            date = PlayerMovement.Instance.GetCurrentDate();
+//            score = PlayerMovement.Instance.finalScore;
+//            SerializeToXml(highScores);
+//        }
+//        if (File.Exists("scores.xml"))
+//        {
+//            highScores = DeSerializeFromXml();
+//        }
+//    }
+
+//    public void SerializeToXml(HighScores highScores)
+//    {
+//        highScores.Scores.Add(new ScoreEntry(date, score));
+//        highScores.Scores = highScores.Scores.OrderByDescending(s => s.Value).ToList();
+//        highScores.Scores = highScores.Scores.Take(7).ToList();
+//        XmlSerializer serializer = new XmlSerializer(typeof(HighScores));
+
+//        using (StreamWriter streamWriter = new StreamWriter("scores.xml"))
+//        {
+//            serializer.Serialize(streamWriter, highScores);
+//        }
+//    }
+
+//    public void DisplayScores()
+//    {
+//        HighScores highScores = DeSerializeFromXml();
+//        foreach (var scoreEntry in highScores.Scores)
+//        {
+//            dateT.text += $"{scoreEntry.Date}\n";
+//            scoreT.text += $"{scoreEntry.Value}\n";
+//        }
+//    }
 
 
-    }
-    private void Update()
-    {
-        if (sceneIndex == 1 && PlayerMovement.Instance.isAlive == false)
-        {
-            SerializeToXml();
-        }
-    }
+//    HighScores DeSerializeFromXml()
+//    {
+//        XmlSerializer serializer = new XmlSerializer(typeof(HighScores));
 
-    public void SerializeToXml()
-    {
-        HighScores highScores = DeSerializeFromXml();
-        highScores.Scores.Add(new ScoreEntry(PlayerMovement.Instance.GetCurrentDate(), PlayerMovement.Instance.finalScore));
-        highScores.Scores = highScores.Scores.OrderByDescending(s => s.Value).ToList();
-        highScores.Scores = highScores.Scores.Take(7).ToList();
-        var serializer = new XmlSerializer(typeof(HighScores));
+//        using (StreamReader streamReader = new StreamReader("scores.xml"))
+//        {
+//            return (HighScores)serializer.Deserialize(streamReader);
+//        }
 
-        // Use Application.persistentDataPath to get a path that works on all platforms
-        string filePath = Path.Combine(Application.persistentDataPath, "Scores.xml");
-
-        using (StreamWriter streamWriter = new StreamWriter(filePath))
-        {
-            serializer.Serialize(streamWriter, highScores);
-        }
-    }
-
-    public void DisplayScores()
-    {
-        HighScores highScores = DeSerializeFromXml();
-        foreach (var scoreEntry in highScores.Scores)
-        {
-            date.text += $"{scoreEntry.Date}\n";
-            score.text += $"{scoreEntry.Value}\n";
-        }
-    }
-
-
-    HighScores DeSerializeFromXml()
-    {
-        XmlSerializer serializer = new XmlSerializer(typeof(HighScores));
-
-        // Use Application.persistentDataPath to get a path that works on all platforms
-        string filePath = Path.Combine(Application.persistentDataPath, "Scores.xml");
-
-        if (File.Exists(filePath))
-        {
-            using (StreamReader streamReader = new StreamReader(filePath))
-            {
-                return (HighScores)serializer.Deserialize(streamReader);
-            }
-        }
-        else
-        {
-            // If the file doesn't exist, return a new instance of HighScores
-            return new HighScores();
-        }
-    }
+//    }
 }
